dd4c46c7cdbe5b38e855708c8ee1d2ba6183d98b
cbaakman <c.baakman@umcn.nl>
2021-11-11 18:11:30 +0100

    fix the test

--- tests/operate/test_pdb.py
+++ tests/operate/test_pdb.py
@@ -1,30 +1,7 @@
 from nose.tools import ok_
 from pdb2sql import pdb2sql
 
-from do.operate.pdb import get_covalent_bonds, get_atoms, get_residue_contact_atom_pairs
-
-
-def test_bonds():
-
-    pdb = pdb2sql("tests/data/pdb/1CRN.pdb")
-    try:
-        atoms = get_atoms(pdb)
-        bonds = get_covalent_bonds(atoms)
-    finally:
-        pdb._close()
-
-    ok_(any([{atom1.name, atom2.name} == {"CA", "CB"} for atom1, atom2 in bonds]))
-
-    ok_(any([{atom1.name, atom2.name} == {"N", "C"} for atom1, atom2 in bonds]))
-
-    ok_(any([{atom1.name, atom2.name} == {"SG", "SG"} for atom1, atom2 in bonds]))
-
-    ok_(not any([{atom1.name, atom2.name} == {"N", "CE"} for atom1, atom2 in bonds]))
-
-    ok_(not any([{atom1.name, atom2.name} == {"CB", "ND2"} for atom1, atom2 in bonds]))
-
-    # Don't bond atoms with themselves !
-    ok_(not any([atom1 == atom2 for atom1, atom2 in bonds]))
+from do.operate.pdb import get_atoms, get_residue_contact_atom_pairs
 
 
 def test_atoms():
